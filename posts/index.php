f0606b81f141155d2d28a52660486e2c34d048c8
Ganz7 <ganzse7en@gmail.com>
2013-03-03 22:49:56 +0530

    Classy as an Olive

--- posts/index.php
+++ posts/index.php
@@ -4,8 +4,11 @@
 <head>
 		<title>Posts | Ganz7</title>
 
-		<link rel="stylesheet" type="text/css" href="/Perspective/css/post_blue_on_white.css"/>
+		<link rel="stylesheet" type="text/css" href="/css/post_blue_on_white.css"/>
 		<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
+
+		<meta name="description" content="Ganesh's home on the web"/>
+        		<meta name="keywords" content="ganz7, ganz7.in, ganesh, kumar, Ganesh kumar, vr, Ganesh Kumar vr, Ganesh V R Kumar, perspective, india, blog, code, about, anime, fracture, naruto, me, coder, hacker, hack, grey,india, chennai, ceg, anna, university" />
 		<!--<script type="text/javascript">
 			$(document).ready(function(){
 				$("#main").scroll(function(){
@@ -14,6 +17,20 @@
 				})
 			})
 		</script>-->
+
+		<link rel="icon" href="/img/fracture_bluee.ico"/>
+		<link rel="shortcut icon" href="/img/fracture_bluee.ico"/>
+
+		<script type="text/javascript"> 
+		      var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+		      document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+		    </script> 
+		    <script type="text/javascript"> 
+		      try {
+		      var pageTracker = _gat._getTracker("UA-29903327-1");
+		      pageTracker._trackPageview();
+		      } catch(err) {}</script>
+
 		
 </head>	
 <body>
@@ -42,13 +59,13 @@
 	</div>
 
 	<div class="footer">
-		<p class="footer_text big"><img src="/Perspective/img/fracture_white.png" height="100px" width="80px"/><br/>Ganz7
+		<p class="footer_text big"><img src="/img/fracture_white.png" height="100px" width="80px"/><br/>Ganz7
 		<table class="footer-table">
 			<tr>
-					<td><a href="" target="_blank" class="link_white medium">About</a></td>
-					<td class="footer_text medium">~</td>
-					<td><a href="" target="_blank" class="link_white medium">Home</a></td>
-					<td class="footer_text medium">~</td>
+					<td><a href="/about.html"  class="link_white medium">About</a></td>
+					<td class="footer_text medium">|</td>
+					<td><a href="/" class="link_white medium">Home</a></td>
+					<td class="footer_text medium">|</td>
 					<td><a href="" target="_blank" class="link_white medium">Code</a></td>
 			</tr>
 		</table>
--- posts/view.php
+++ posts/view.php
@@ -1,8 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="stylesheet" type="text/css" href="/Perspective/css/post_blue_on_white.css"/>
+	<link rel="stylesheet" type="text/css" href="/css/post_blue_on_white.css"/>
 
+	<link rel="icon" href="/img/fracture_bluee.ico"/>
+	<link rel="shortcut icon" href="/img/fracture_bluee.ico"/>
+
+	<script type="text/javascript"> 
+	      var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+	      document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+	    </script> 
+	    <script type="text/javascript"> 
+	      try {
+	      var pageTracker = _gat._getTracker("UA-29903327-1");
+	      pageTracker._trackPageview();
+	      } catch(err) {}</script>
 
 	<?php
 				require('connect.php');	
@@ -31,8 +43,10 @@
 				$post_rec=mysql_fetch_array($query);
 				$blogpost = $post_rec['post'];
 				$blogtitle = $post_rec['title'];
+				$timeline = $post_rec['timeline'];
 				
-				echo "<p class=\"post_title\">$blogtitle</p>";			
+				echo "<p class=\"post_title\">$blogtitle</p>";
+				echo "<p class=\"post_timeline\">$timeline</p>";		
 			?>
 		<hr/>	
 		</div>
@@ -46,13 +60,13 @@
 	</div>
 
 	<div class="footer">
-		<p class="footer_text big"><img src="/Perspective/img/fracture_white.png" height="100px" width="80px"/><br/>Ganz7
+		<p class="footer_text big"><img src="/img/fracture_white.png" height="100px" width="80px"/><br/>Ganz7
 		<table class="footer-table">
 			<tr>
-					<td><a href="" target="_blank" class="link_white medium">About</a></td>
-					<td class="footer_text medium">~</td>
-					<td><a href="" target="_blank" class="link_white medium">Home</a></td>
-					<td class="footer_text medium">~</td>
+					<td><a href="/about.html" class="link_white medium">About</a></td>
+					<td class="footer_text medium">|</td>
+					<td><a href="/"  class="link_white medium">Home</a></td>
+					<td class="footer_text medium">|</td>
 					<td><a href="" target="_blank" class="link_white medium">Code</a></td>
 			</tr>
 		</table>
